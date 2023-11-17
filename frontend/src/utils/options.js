7698cdd1706f6b317fb377859994a69a8be3f6e4
Dmitry Uchkin <not3hunnit@icloud.com>
2023-11-18 02:06:49 +0300

    Done

--- frontend/src/utils/options.js
+++ frontend/src/utils/options.js
@@ -1,12 +1,12 @@
-// export const apiOptions = {
-//     baseUrl: 'https://api.mesto.haku.nomoredomainsmonster.ru',
-//     headers: {
-//         "Content-Type": 'application/json',
-//     }
-// };
 export const apiOptions = {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://api.mesto.haku.nomoredomainsmonster.ru',
     headers: {
         "Content-Type": 'application/json',
     }
 };
+// export const apiOptions = {
+//     baseUrl: 'http://localhost:3000',
+//     headers: {
+//         "Content-Type": 'application/json',
+//     }
+// };
